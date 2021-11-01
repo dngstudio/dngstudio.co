19dec67bca640d267fa22593ab2da8458fdd948f
Jelena Janic <office@dngstudio.co>
2021-11-01 09:35:28 +0100

    Form

--- index.php
+++ index.php
@@ -220,33 +220,9 @@ require './modules/header.php'; ?>
 </div>
 
 
-<div class="container d-flex align-items-start justify-content-center mt-5">
-  <div class="card shadow mx-2 py-3 my-auto">
-    <div class="card-body p-3">
-      <h3 class="card-title">Ne uklapaš se u pakete?</h3>
-      <p class="card-text my-2">To je sasvim normalno! Svaki projekat je priča za sebe i ne treba svakom klijentu isti tip usluge. Slobodno nam napiši u formi ispod šta te zanima, a mi ćemo da te provedemo kroz neka najbitnija pitanja. Ovde možeš da odlučiš da li želiš da se prvo konsultujemo ili bi odmah da nas angažuješ.</p>
-      <div class="row">
-        <div class="col">
-          <div id="builder"></div>
-        </div>
-        <div class="col">
-          <div class="card mb-3 info bg-light" style="max-width: 18rem;">
-            <div class="card-body">
-              <h4 class="card-title"><i class="bi bi-info-circle me-1" aria-label="Info"></i>Nekoliko napomena</h4>
-              <ul>
-                <li>Od kompleksnosti projekta najviše zavise cena i rok izrade</li>
-                <li>Popuni formu što preciznije</li>
-                <li>Obavezno ostavi pravu mejl adresu</li>
-                <li>Konsultacije se vrše online, a po potrebi se zakazuje sastanak</li>
-                <li>Za sve nejasnoće obrati nam se putem četa ili poziva</li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div> 
-    </div>
-  </div>
-</div>
+
+<?php include './modules/contact.php' ?>
+
 
 
 <div class="container mt-5" id="faq">
