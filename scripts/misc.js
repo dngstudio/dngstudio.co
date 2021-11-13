4e4c0fe4f29378c0562e9e1b54e749ff3c3c7864
Jelena Janic <office@dngstudio.co>
2021-11-13 13:34:16 +0100

    Overflow

--- scripts/misc.js
+++ scripts/misc.js
@@ -96,3 +96,13 @@ picker.addEventListener('input', function(e){
   }
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+          behavior: 'smooth'
+      });
+  });
+});
+
